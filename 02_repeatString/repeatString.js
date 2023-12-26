d1782c2fd8f5041fa613b4fde98726d4f4e751af
Sean Uz <cuz3@gatech.edu>
2023-12-25 20:32:34 -0500

    Solve exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(str, count) {
+    if (count < 0 || typeof count !== "number") return "ERROR"; 
+    retval = "";
+    for (let i = 0; i < count; i++) {
+        retval += str;
+    }
 
+    return retval;
 };
 
 // Do not edit below this line
