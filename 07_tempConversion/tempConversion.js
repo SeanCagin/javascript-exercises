7b505a1d9e9cfe8763af2a47332cd6e4dbbdbe80
Sean Uz <cuz3@gatech.edu>
2023-12-25 21:18:09 -0500

    Complete exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
-const convertToCelsius = function() {
+function isNum(value) {
+  if (typeof value !== "number" || isNaN(value)) return false;
+  return true;
+}
+
+const convertToCelsius = function(degrees) {
+  if (!isNum(degrees)) return "ERROR";
+  return +((degrees - 32) * 5 / 9).toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degrees) {
+  if (!isNum(degrees)) return "ERROR";
+  return +(degrees * 9 / 5 + 32).toFixed(1);
 };
 
 // Do not edit below this line
