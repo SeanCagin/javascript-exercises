ba41ef122d9ff4e5cb5648898709c9410b7a34fb
Sean Uz <cuz3@gatech.edu>
2023-12-25 21:03:20 -0500

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(lower, upper) {
+    if (typeof lower !== "number" || typeof upper !== "number" || 
+        isNaN(lower) || isNaN(upper) || 
+        lower < 0 || upper < 0) {
+            return "ERROR";
+        }
+        
+    if (lower > upper) {
+        [lower, upper] = [upper, lower];
+    }
+    
+    retval = 0;
+    for (let i = lower; i <= upper; i++) {
+        retval += i;
+    }
+    
+    return retval;
 };
 
 // Do not edit below this line
