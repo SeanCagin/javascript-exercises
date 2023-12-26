b92960610184e2c135f4bdb05e6c38154cae0c63
Sean Uz <cuz3@gatech.edu>
2023-12-25 20:35:26 -0500

    Solve exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    if (typeof str !== "string") return "ERROR";
+    retval = "";
+    let i = str.length - 1;
+    while (i >= 0) {
+        retval += str.charAt(i);
+        i--;
+    }
+    return retval;
 };
 
 // Do not edit below this line
