09991d1b1b236b5d70b15637e9bfeead67445903
Sean Uz <cuz3@gatech.edu>
2023-12-25 21:07:20 -0500

    Complete exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (typeof year !== "number" || year < 0 || isNaN(year)) return "ERROR";
+    if (year % 4 != 0) {
+        return false;
+    } else if (year % 100 == 0) {
+        if (year % 400 == 0) return true;
+        return false;
+    }
+    return true;
 };
 
 // Do not edit below this line
