d1b27fe349b3d5b9dcca097f9bea586de9c937be
Sean Uz <cuz3@gatech.edu>
2023-12-25 20:58:47 -0500

    Complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...options) {
+    let retval = [];
+    let delItems = new Set(options);
+    arr.forEach(element => {
+        if (!(delItems.has(element))) {
+            retval.push(element);
+        }
+    });
+    return retval;
 
 };
 
+
+removeFromArray([2, 4], 2, 5);
 // Do not edit below this line
 module.exports = removeFromArray;
