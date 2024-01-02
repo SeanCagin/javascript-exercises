0df55a9f00385f8590cb73dda5b2119c7ae71c81
Sean Uz <cuz3@gatech.edu>
2024-01-01 21:59:15 -0500

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    n = +n;
+    if (n < 1) return "OOPS";
+    let first = 1;
+    let second = 1;
+    let i = 2;
+    while (i < n) {
+        first = first + second;
+        second = second + first;
+        i += 2;
+    }
+    return i == n ? second : first;
 };
 
 // Do not edit below this line
