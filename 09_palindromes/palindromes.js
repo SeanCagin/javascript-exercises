67b01367c3668d5e2e39abfdd3bad2f2a999ebba
Sean Uz <cuz3@gatech.edu>
2024-01-01 21:55:54 -0500

    Complete exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,25 @@
-const palindromes = function () {
+function movePointer(string, pointer, direction) {
+    let invalidChars = new Set(["!", "?", ",", ".", " ", "-"]);
+    while(invalidChars.has(string.charAt(pointer))) {
+        pointer += direction;
+    }
+    return pointer;
+}
 
+const palindromes = function (str) {
+    str = str.toUpperCase();
+    let front = 0;
+    let back = str.length - 1;
+    while (front < back) {
+        front = movePointer(str, front, 1);
+        back = movePointer(str, back, -1);
+        if (str.charAt(front) !== str.charAt(back)) return false;
+        front++;
+        back--;
+    }
+    return true;
 };
 
 // Do not edit below this line
+palindromes("A car, a man, a maraca.");
 module.exports = palindromes;
