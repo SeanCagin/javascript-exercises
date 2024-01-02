95dfbb2d62a5075fb42838e44de60087d37cb87f
Sean Uz <cuz3@gatech.edu>
2024-01-01 22:31:09 -0500

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+function getAge(person) {
+    let retval = "yearOfDeath" in person ?
+        person.yearOfDeath - person.yearOfBirth : 
+        new Date().getFullYear() - person.yearOfBirth;
+    
+    console.log(retval);
+    return retval;
+}
 
+const findTheOldest = function(people) {
+    return people.reduce((acc, curr) => {
+        return getAge(curr) > getAge(acc) ? curr : acc;
+    });
 };
 
 // Do not edit below this line
